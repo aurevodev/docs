c559264e5855a9e4b0d448df32dcff104ce47a3c
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-08-27 22:52:28 +0600

    feat: self-hosted fonts less tracking

--- pages/_document.js
+++ pages/_document.js
@@ -7,14 +7,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=optional"
+            href="https://aurevocdn.xyz/fonts/inter-font/inter.css"
             rel="stylesheet"
           />
         </Head>
